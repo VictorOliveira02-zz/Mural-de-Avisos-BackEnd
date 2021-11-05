2a4a9b2639eecb202c441826e2f4e08fce9b14e3
VictorOliveira02 <victoralvees17@gmail.com>
2021-11-05 10:16:59 -0300

    ajustes rotas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const apiRoute = require('./routes/api');
 
-const PORT = 5000
+const PORT = "https://mural-de-avisos-back-end.vercel.app/"
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT);
 })
