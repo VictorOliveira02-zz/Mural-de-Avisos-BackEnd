d342c746f300371423823c23f2dde50a25ed970d
VictorOliveira02 <victoralvees17@gmail.com>
2021-11-05 20:48:49 -0300

    ajustes

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const apiRoute = require('./routes/api');
 app.use(cors());
 
 
-app.listen("https://mural-de-avisos-backend.herokuapp.com")
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+})
 
 app.use('/api', apiRoute);
