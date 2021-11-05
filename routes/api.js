7e461eb86c5342e909d052e5ae38a00c615ffcf2
VictorOliveira02 <victoralvees17@gmail.com>
2021-11-05 10:42:27 -0300

    try to enable CORS

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,15 @@ router.use(cors(accessibleUrl));
 
 const posts = require('../model/posts');
 
+router.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x - client - key, x - client - token, x - client - secret, Authorization");
+    next();
+});
+
+
 router.get('/all', (req, res) => {
     res.send(posts.getAll());
 })
